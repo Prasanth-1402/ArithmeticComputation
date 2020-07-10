882f64365437708fd84541a577fcfdc1ada47fb9
Prasanth-Shanmugam <prasanthshanmugam1402@gmail.com>
2020-07-10 14:53:50 +0530

    [Prasanth] Data: Dictionary->Array

--- arithmetic_comp_sort.sh
+++ arithmetic_comp_sort.sh
@@ -1,21 +1,45 @@
 #!/usr/local/bin/bash -x
 declare -A resultContainerDictionary
+decalre -a resultContainerArray
 read -p "Get a:" a
 read -p "Get b:" b
 read -p "Get c:" c
 
+#computation
 result=$(($((a+b))*$c))
 result1=$(($((c+b))*$a))
 result2=$(($((a/b))+$c))
 result3=$(($((a%b))+$c))
 
+#Putting the computed values inside a dictionary
 resultContainerDictionary[FirstResult]=$result
 resultContainerDictionary[SecondResult]=$result1
 resultContainerDictionary[ThirdResult]=$result2
 resultContainerDictionary[FourthResult]=$result3
 
 
+#storing data from dictionary to array
+resultContainerArray[0]=$result
+resultContainerArray[1]=$result1
+resultContainerArray[2]=$result2
+resultContainerArray[3]=$result3
+
+
+
+
+
+
+echo SHOWING RESULTS FROM DICTIONARY
+
 echo First_Result=${resultContainerDictionary[FirstResult]}
 echo Second_Result=${resultContainerDictionary[SecondResult]}
 echo Third_Result=${resultContainerDictionary[ThirdResult]}
 echo Fourth_Result=${resultContainerDictionary[FourthResult]}
+
+
+echo SHOWING RESULTS FROM ARRAY
+
+for((i=0;$i<4;i++))
+do
+	echo ${resultContainerArray[$i]}
+done
